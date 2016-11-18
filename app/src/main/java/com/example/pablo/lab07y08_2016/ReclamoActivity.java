050204bd7c7501f257ea4ef1860b8e15f4b644ac
nightpablo <nightpablo1991@gmail.com>
2016-11-18 20:15:53 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pablo/lab07y08_2016/ReclamoActivity.java
@@ -0,0 +1,34 @@
+package com.example.pablo.lab07y08_2016;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class ReclamoActivity extends AppCompatActivity implements com.google.android.gms.maps.OnMapReadyCallback{
+
+    private GoogleMap myMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reclamo);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        myMap = googleMap;
+    }
+}
