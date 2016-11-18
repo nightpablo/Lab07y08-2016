23c6cd1799f6847ff5693727fe0fd627027bd9c4
nightpablo <nightpablo1991@gmail.com>
2016-11-18 20:50:49 -0300

    Hecho hasta encontrar mi ubicacion (5.1)

--- app/src/main/java/com/example/pablo/lab07y08_2016/ReclamoActivity.java
+++ app/src/main/java/com/example/pablo/lab07y08_2016/ReclamoActivity.java
@@ -1,8 +1,14 @@
 package com.example.pablo.lab07y08_2016;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -30,5 +36,20 @@ public class ReclamoActivity extends AppCompatActivity implements com.google.and
     @Override
     public void onMapReady(GoogleMap googleMap) {
         myMap = googleMap;
+        actualizarMapa();
+    }
+
+    private void actualizarMapa(){
+        if (ContextCompat.checkSelfPermission(ReclamoActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},9999);
+            return;
+        }
+        myMap.setMyLocationEnabled(true);
+
+
     }
 }
