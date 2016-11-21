c78299e30f71ff73779d90709cf8f40adf9cf97c
nightpablo <nightpablo1991@gmail.com>
2016-11-21 19:00:18 -0300

    Hecho hasta 6.4

--- app/src/main/java/com/example/pablo/lab07y08_2016/ReclamoActivity.java
+++ app/src/main/java/com/example/pablo/lab07y08_2016/ReclamoActivity.java
@@ -2,8 +2,12 @@ package com.example.pablo.lab07y08_2016;
 
 import android.*;
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,14 +16,19 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.pablo.lab07y08_2016.model.Reclamo;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -55,6 +64,64 @@ public class ReclamoActivity extends AppCompatActivity implements com.google.and
                 startActivityForResult(i,CODIGO_RESULTADO_ALTA_RECLAMO);
             }
         });
+        myMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(final Marker marker) {
+
+                final Dialog miDialogo = new Dialog(ReclamoActivity.this);
+                miDialogo.setContentView(R.layout.alert_distancia_busqueda);
+                miDialogo.setTitle("Reclamos a localizar");
+                final EditText distanciaReclamo = (EditText) miDialogo.findViewById(R.id.distanciaReclamo);
+                Button btnBuscar = (Button) miDialogo.findViewById(R.id.button_Buscar);
+                Button btnCancelar = (Button) miDialogo.findViewById(R.id.button_Cancelar);
+
+                btnBuscar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        float[] Resultados = new float[10];
+                        ArrayList<Reclamo> listaEncontrados = new ArrayList<Reclamo>();
+                        for(Reclamo i: listaReclamos){
+                            if(i.getLatitud()!=marker.getPosition().latitude && i.getLongitud()!=marker.getPosition().longitude) {
+                                Resultados = new float[10];
+                                Location.distanceBetween(i.getLatitud(),i.getLongitud(),
+                                        marker.getPosition().latitude,marker.getPosition().longitude,Resultados);
+                                float distanciaCalculada = Resultados[0];
+                                if(distanciaCalculada<Float.parseFloat(distanciaReclamo.getText().toString()))
+                                    listaEncontrados.add(i);
+
+                            }
+                        }
+                        /*Location.distanceBetween(listaReclamos.get(0).getLatitud(),listaReclamos.get(0).getLongitud()
+                        ,listaReclamos.get(1).getLatitud(),listaReclamos.get(1).getLongitud(),Resultados); *///Retorna en metros
+                        
+                        miDialogo.dismiss();
+                    }
+                });
+
+                btnCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        miDialogo.dismiss();
+                    }
+                });
+
+                miDialogo.show();
+
+
+                /*new AlertDialog.Builder(ReclamoActivity.this) //Ventana de Dialogo
+                        .setView(LayoutInflater.from(ReclamoActivity.this).inflate(R.layout.alert_distancia_busqueda,null))
+                        .setMessage("Reclamos a calcular")
+                        .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Accion del punto 6.3
+                            }
+                        })
+                        .setNegativeButton("Cancelar",null)
+                        .create()
+                        .show();*/
+            }
+        });
         localizarMiPosicion();
     }
 
@@ -67,6 +134,7 @@ public class ReclamoActivity extends AppCompatActivity implements com.google.and
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},9999);
             return;
         }
+
         myMap.setMyLocationEnabled(true);
 
 
@@ -80,6 +148,7 @@ public class ReclamoActivity extends AppCompatActivity implements com.google.and
             .position(nuevoReclamo.coordenadaUbicacion())
             .title("Reclamo de "+nuevoReclamo.getEmail())
             .snippet(nuevoReclamo.getTitulo()));
+
             listaReclamos.add(nuevoReclamo);
         }
     }
--- app/src/main/java/com/example/pablo/lab07y08_2016/model/Reclamo.java
+++ app/src/main/java/com/example/pablo/lab07y08_2016/model/Reclamo.java
@@ -13,6 +13,8 @@ import java.io.Serializable;
  */
 
 public class Reclamo implements Serializable{
+
+
     private Double latitud;
     private Double longitud;
     private String titulo;
@@ -68,4 +70,11 @@ public class Reclamo implements Serializable{
         this.imagenPath = imagenPath;
     }
 
+    public Double getLatitud() {return latitud;}
+
+    public void setLatitud(Double latitud) {this.latitud = latitud;}
+
+    public Double getLongitud() {return longitud;}
+
+    public void setLongitud(Double longitud) {this.longitud = longitud;}
 }
